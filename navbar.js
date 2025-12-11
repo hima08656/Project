57d94ac6174c25c29c63559aec5c9c2979b20088
Hima Krishna Priy <yadlahimakrishnapriya@gmail.com>
2025-12-11 19:44:29 +0530

    navbar

---
+++ navbar.js
@@ -0,0 +1,11 @@
+// components/navbar.js
+export function Navbar() {
+    return `
+    <nav class="navbar">
+      <a href="/pages/index.html">Home</a>
+      <a href="/pages/signup.html">Signup</a>
+      <a href="/pages/login.html">Login</a>
+      <a href="/pages/todos.html">Todos</a>
+    </nav>
+  `;
+}
